2b91038e4da8395b121144676d209d49ce105482
Sandroware7 <sandro.rodriguez.b@tecsup.edu.pe>
2026-05-07 22:38:39 -0500

    Responsable: Actualizacion de StudentController.java

--- src/StudentController.java
+++ src/StudentController.java
@@ -3,5 +3,7 @@ public class StudentController {
     public void controller() {
         // TO DO
     }
-
+    public void mostrarMensaje() {
+        System.out.println("Hola, soy el Responsable en sprint - 1");
+    }
 }

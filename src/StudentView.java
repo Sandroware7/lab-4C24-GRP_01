e0dd1ca4bf17984b69589aa3b800b4342e8391a4
Sandroware7 <sandro.rodriguez.b@tecsup.edu.pe>
2026-05-09 23:27:42 -0500

    Corrección en StudentView

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class StudentView {
 
 
@@ -6,9 +5,3 @@ public class StudentView {
         System.out.println("Vista de estudiantes actualizada");
     }
 }
-=======
-public void mostrarMensaje() {
-    System.out.println("Vista de estudiante actualizada");
-    System.out.println("Los cambios se han guardado exitosamente");
-}
->>>>>>> 29c056615a30b2f8d1ee21f1e0d7e5c948ba1803
